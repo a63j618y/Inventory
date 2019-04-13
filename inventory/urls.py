b01f58d9b8536be6b6c239f60a9ec9bcf8fe66e0
Naoki Shinoda <naoki_shinoda@example.com>
2019-04-13 20:43:45 +0900

    add template and css

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('<int:id>/<nickname>/', views.index, name='index'),
+    # path('<int:id>/<nickname>/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('next', views.next, name='next'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request, id, nickname):
+# def index(request, id, nickname):
+def index(request):
     # if 'id' in request.GET:
     #     id = request.GET['id']
     #     result = 'you typed : "' + id + '".'
     # else:
     #     result = 'please send msg parameter'
-    result = 'your id: ' + str(id) + ', name: "' + nickname + '".'
-    return HttpResponse(result)
+    # result = 'your id: ' + str(id) + ', name: "' + nickname + '".'
+    params = {
+        'title': 'Hello/Index',
+        'msg': 'これはサンプル',
+        'goto': 'next',
+    }
+    # return HttpResponse(request, 'hello/index.html')
+    return render(request, 'inventory/index.html', params)
 
+
+def next(request):
+    params = {
+        'title': 'hello/Next',
+        'msg': 'これは、次のページ',
+        'goto': 'index',
+    }
+    return render(request, 'inventory/index.html', params)
