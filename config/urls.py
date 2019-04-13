7823deef112133d9fd3fe645792e633adab671c2
Naoki Shinoda <naoki_shinoda@example.com>
2019-04-13 19:22:42 +0900

    add view

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import inventory.views as inventory
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inventory/', include('inventory.urls')),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request, id, nickname):
+    # if 'id' in request.GET:
+    #     id = request.GET['id']
+    #     result = 'you typed : "' + id + '".'
+    # else:
+    #     result = 'please send msg parameter'
+    result = 'your id: ' + str(id) + ', name: "' + nickname + '".'
+    return HttpResponse(result)
+
